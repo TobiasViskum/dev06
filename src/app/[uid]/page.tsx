eda50473fb40beb53a6b2f4d8a089a95374a48af
TobiasViskum <tobias.t.viskum@gmail.com>
2023-06-02 21:12:21 +0200

    fixed

--- src/app/[uid]/page.tsx
+++ src/app/[uid]/page.tsx
@@ -4,6 +4,6 @@ export const metadata: Metadata = {
   title: "Start",
 };
 
-export default async function Start({ uid }: { uid: string }) {
-  return <h1>This is your entered uid: {uid}</h1>;
+export default async function Start({ params }: { params: { uid: string } }) {
+  return <h1>This is your entered uid: {params.uid}</h1>;
 }
